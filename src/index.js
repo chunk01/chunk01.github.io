72d41da09cb2a6a84097af2038eaf02146213631
Luca Marsman <59541889+chunk01@users.noreply.github.com>
2023-03-03 17:48:39 -0500

    Add event listeners to default project button

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ import {init} from "./init";
 
 init();
 
+document.getElementById("inbox").addEventListener("click", function () {
+    contentPopulate("Inbox");
+});
+
+document.getElementById("today").addEventListener("click", function () {
+    contentPopulate("Today");
+});
+
+document.getElementById("week").addEventListener("click", function () {
+    contentPopulate("This Week");
+});
