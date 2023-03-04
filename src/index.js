9cc6834609897c63b0bbf953974a55a8bb5920c8
Luca Marsman <59541889+chunk01@users.noreply.github.com>
2023-03-03 21:26:14 -0500

    Make content populate with home project on load

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import {init} from "./init";
+import {contentPopulate} from "./contentPopulate";
 
 init();
+contentPopulate("Inbox");
 
 document.getElementById("inbox").addEventListener("click", function () {
+    document.getElementById("project").innerHTML = "";
     contentPopulate("Inbox");
 });
 
 document.getElementById("today").addEventListener("click", function () {
-    contentPopulate("Today");
+    document.getElementById("project").innerHTML = "";
+    displayTasksToday();
 });
 
 document.getElementById("week").addEventListener("click", function () {
-    contentPopulate("This Week");
+    document.getElementById("project").innerHTML = "";
+    displayTasksWeek();
 });
