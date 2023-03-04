f784275b1ac572b3aa0506c128fb4939eace13bd
Luca Marsman <59541889+chunk01@users.noreply.github.com>
2023-03-04 16:49:55 -0500

    Make add task popup interact with add button

--- src/init.js
+++ src/init.js
@@ -16,16 +16,59 @@ const init = () => {
     let inbox = document.createElement("button");
     inbox.setAttribute("id", "inbox");
     inbox.innerText = "Inbox";
+    inbox.classList.add("active");
     let day = document.createElement("button");
     day.setAttribute("id", "today");
     day.innerText = "Today";
     let week = document.createElement("button");
     week.setAttribute("id", "week");
     week.innerText = "This Week";
+
+    inbox.addEventListener("click", function () {
+        //TODO - remove active class from all other projects
+        inbox.classList.add("active");
+    })
+
+
     defaultProjects.appendChild(inbox);
     defaultProjects.appendChild(day);
     defaultProjects.appendChild(week);
     dash.appendChild(defaultProjects);
+    let customProjectHeader = document.createElement("h2");
+    customProjectHeader.innerText = "Projects";
+    let customProjectList = document.createElement("div");
+    customProjectList.setAttribute("id", "customProjectList");
+    let addProject = document.createElement("button");
+    addProject.setAttribute("id", "addProject");
+    addProject.innerText = "Add Project";
+    dash.appendChild(customProjectHeader);
+    dash.appendChild(customProjectList);
+    dash.appendChild(addProject);
+    let buttonHolderDash = document.createElement("div");
+    buttonHolderDash.setAttribute("id", "buttonContainerDash");
+    let add = document.createElement("button");
+    add.setAttribute("id", "addProject");
+    add.innerText = "Add";
+    let cancel = document.createElement("button");
+    cancel.setAttribute("id", "cancelProject");
+    cancel.innerText = "Cancel";
+    buttonHolderDash.appendChild(add);
+    buttonHolderDash.appendChild(cancel);
+
+    addProject.addEventListener("click", function () {
+        document.getElementById("addProject").style.visibility = "hidden";
+        dash.appendChild(buttonHolderDash);
+    })
+
+    add.addEventListener("click", function () {
+        //insert project into list
+    });
+
+    cancel.addEventListener("click", function () {
+        document.getElementById("buttonContainerDash").style.visibility = "hidden";
+        document.getElementById("addProject").style.visibility = "";
+    });
+    
 
     let innerContent = document.createElement("div");
     innerContent.setAttribute("id", "innerContent");
