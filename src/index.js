a5cab5951cc3d361c138805d39cefd191385f9bb
Luca Marsman <59541889+chunk01@users.noreply.github.com>
2023-03-04 21:03:53 -0500

    Add logic to update active project

--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ const inbox = new Project("Inbox");
 contentPopulate(inbox);
 
 document.getElementById("inbox").addEventListener("click", function () {
+    document.getElementById("inbox").classList.add("active")
+    let customProjects = document.getElementById("customProjectList").childNodes;
+            for(let i = 0; i < customProjects.length; i++) {
+                if (customProjects[i] != document.getElementById("inbox")) {
+                    customProjects[i].classList.remove("active");
+                }
+            }
     document.getElementById("project").innerHTML = "";
     contentPopulate(inbox);
 });
