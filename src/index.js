27f25a8bb7be8d32f0d34abd6928e88297a1126a
Luca Marsman <59541889+chunk01@users.noreply.github.com>
2023-03-06 18:51:25 -0500

    Add active class update for dash buttons

--- src/index.js
+++ src/index.js
@@ -13,21 +13,59 @@ contentPopulate(inbox);
 document.getElementById("inbox").addEventListener("click", function () {
     document.getElementById("inbox").classList.add("active");
     let customProjects = document.getElementById("customProjectList").childNodes;
-            for(let i = 0; i < customProjects.length; i++) {
-                if (customProjects[i] != document.getElementById("inbox")) {
-                    customProjects[i].classList.remove("active");
-                }
-            }
+    for(let i = 0; i < customProjects.length; i++) {
+        if (customProjects[i] != document.getElementById("inbox")) {
+            customProjects[i].classList.remove("active");
+        }
+    }
+
+    let defaultProjects = document.getElementById("default").childNodes;
+    for(let i = 0; i < defaultProjects.length; i++) {
+        if (defaultProjects[i] != document.getElementById("inbox")) {
+            defaultProjects[i].classList.remove("active");
+        }
+    }
+
     document.getElementById("project").innerHTML = "";
     contentPopulate(inbox);
 });
 
 document.getElementById("today").addEventListener("click", function () {
+    document.getElementById("today").classList.add("active");
+    let customProjects = document.getElementById("customProjectList").childNodes;
+    for(let i = 0; i < customProjects.length; i++) {
+        if (customProjects[i] != document.getElementById("today")) {
+            customProjects[i].classList.remove("active");
+        }
+    }
+
+    let defaultProjects = document.getElementById("default").childNodes;
+    for(let i = 0; i < defaultProjects.length; i++) {
+        if (defaultProjects[i] != document.getElementById("today")) {
+            defaultProjects[i].classList.remove("active");
+        }
+    }
+
     document.getElementById("project").innerHTML = "";
     displayTasksToday();
 });
 
 document.getElementById("week").addEventListener("click", function () {
+    document.getElementById("week").classList.add("active");
+    let customProjects = document.getElementById("customProjectList").childNodes;
+    for(let i = 0; i < customProjects.length; i++) {
+        if (customProjects[i] != document.getElementById("week")) {
+            customProjects[i].classList.remove("active");
+        }
+    }
+
+    let defaultProjects = document.getElementById("default").childNodes;
+    for(let i = 0; i < defaultProjects.length; i++) {
+        if (defaultProjects[i] != document.getElementById("week")) {
+            defaultProjects[i].classList.remove("active");
+        }
+    }
+
     document.getElementById("project").innerHTML = "";
     displayTasksWeek();
 });
