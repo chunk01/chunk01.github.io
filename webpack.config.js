b925e4752499110306491b00473db90887e953d2
chunk01 <lucamarsman@live.com>
2023-10-17 19:14:28 -0400

    Update dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/dist'),
   },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, '/dist')
+    }
+}
 };
